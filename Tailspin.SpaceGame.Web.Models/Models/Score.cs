a33456a7a1d22dba3c53c3857026b4c0114e689e
Ciesielski <jzci@gft.com>
2020-12-18 12:38:16 +0100

    Add GamStyle property

--- Tailspin.SpaceGame.Web.Models/Models/Score.cs
+++ Tailspin.SpaceGame.Web.Models/Models/Score.cs
@@ -19,5 +19,9 @@ namespace TailSpin.SpaceGame.Web.Models
         // The game region (map) the score is associated with.
         [JsonProperty(PropertyName = "gameRegion")]
         public string GameRegion { get; set; }
+
+        //The game style (difficulty) the score is associated with.
+        [JsonProperty(PropertyName = "game-style")]
+        public string GameStyle { get; set; }
     }
 }
